c138c92376a807e2dc6dd933a1b235a631378d32
jenous11 <jenousdongol11@gmail.com>
2026-04-03 21:47:01 +0545

    did route model binding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,13 +8,14 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function show($id)
+    public function show(Post $post)  // Route Model Binding actual thing to learn
+    // tyupe hinting laravel will automatically resolve the post by id from URL
     {
-        $post=Post::findOrFail($id); // find the post by id from URL if not found it will throw 404 error_clear_last
+        // $post=Post::findOrFail($id); // find the post by id from URL if not found it will throw 404 error_clear_last
         return view('post',compact('post')); // sends it to the view
         //what compact does
         // It takes a string of the variable name and automatically creates the array for you:
-          
+
         // old way you already know
 // return view('post', ['post' => $post]);
     }
