fa528da44c6890b4ddfee6dd78043729a02db55c
jenous11 <jenousdongol11@gmail.com>
2026-05-18 17:26:45 +0545

    added login/logout in the navbar and all posts in navbar of dashboard for seeing all posts from all users

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -12,6 +12,7 @@
     <a href="{{ route('home') }}" class="text-2xl font-bold text-gray-800"> Blog</a>
     <nav>
         <ul class="flex space-x-4">
+            <li><a href="{{ route('dashboard') }}" class="text-gray-600 hover:text-gray-800">Login/Logout</a></li>
             <li><a href="{{ route('posts.create') }}" class="text-gray-600 hover:text-gray-800">Create</a></li>
             <li><a href="{{ route('about') }}" class="text-gray-600 hover:text-gray-800">About Us</a></li>
             <li><a href="{{ route('contact') }}" class="text-gray-600 hover:text-gray-800">Contact</a></li>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('My Posts') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -21,9 +21,13 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
+
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                      <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <ul>
+                          <li><a href="{{ route('posts.index') }}" class="text-gray-600 hover:text-gray-800 display flex list-none mr-5">All Posts</a></li>
+                        </ul>
+                        <div>{{ Auth::user()->name }} </div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
