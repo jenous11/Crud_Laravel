a9e0c61776a365ebf18806e70b40739dce2a20f5
jenous11 <jenousdongol11@gmail.com>
2026-05-18 23:47:58 +0545

    made user roles for admin and user in user table and made a middleware AdminMiddleware

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Homepage</title>
     <script src="https://cdn.tailwindcss.com"></script>
-          @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-gray-100">
-<header class="bg-white shadow-md p-4 flex justify-between items-center container mx-auto mt-6 rounded-lg">
-    <a href="{{ route('home') }}" class="text-2xl font-bold text-gray-800"> Blog</a>
-    <nav>
-        <ul class="flex space-x-4">
+    <header class="bg-white shadow-md p-4 flex justify-between items-center container mx-auto mt-6 rounded-lg">
+        <a href="{{ route('home') }}" class="text-2xl font-bold text-gray-800"> Blog</a>
+        <nav>
+            <ul class="flex space-x-4">
 
-           @auth
-    <li><a href="{{ route('dashboard') }}" class="text-gray-600 hover:text-gray-800">{{ Auth::user()->name }}</a></li>
-    <li><a href="{{ route('posts.create') }}" class="text-gray-600 hover:text-gray-800">Create</a></li>
-@else
-    <li><a href="{{ route('login') }}" class="text-gray-600 hover:text-gray-800">Login</a></li>
-@endauth
+                @auth
+                    <li><a href="{{ route('dashboard') }}"
+                            class="text-gray-600 hover:text-gray-800">{{ Auth::user()->name }}</a></li>
+                    <li><a href="{{ route('posts.create') }}" class="text-gray-600 hover:text-gray-800">Create</a></li>
+                @else
+                    <li><a href="{{ route('login') }}" class="text-gray-600 hover:text-gray-800">Login</a></li>
+                @endauth
 
-            <li><a href="{{ route('about') }}" class="text-gray-600 hover:text-gray-800">About Us</a></li>
-            <li><a href="{{ route('contact') }}" class="text-gray-600 hover:text-gray-800">Contact</a></li>
-        </ul>
-    </nav>
-</header>
-@yield('content')
+                <li><a href="{{ route('about') }}" class="text-gray-600 hover:text-gray-800">About Us</a></li>
+                <li><a href="{{ route('contact') }}" class="text-gray-600 hover:text-gray-800">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    @yield('content')
 </body>
+
 </html>
